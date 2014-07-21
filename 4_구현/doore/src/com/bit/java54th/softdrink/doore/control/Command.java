7c6f7175a3ffc10548a70b31117f476af6f8abaa
trajeta <wonsokj@gmail.com>
2014-07-21 11:28:04 +0900

    정리3

--- 4_구현/doore/src/com/bit/java54th/softdrink/doore/control/Command.java
+++
@@ -1,7 +0,0 @@
-package com.bit.java54th.softdrink.doore.control;
-
-import javax.servlet.http.*;
-
-public interface Command {
-	public CommandResult execute(HttpServletRequest request, HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException;
-}
